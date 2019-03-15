ccaadc0bd81369bd3751aa3e30edc446469a7858
HoangND <hoangnguyenit98@gmail.com>
2019-03-15 13:44:33 +0700

    update LoaihangTable
    Signed-off-by: HoangND <hoangnguyenit98@gmail.com>

--- src/Model/Table/LoaihangTable.php
+++ src/Model/Table/LoaihangTable.php
@@ -6,29 +6,9 @@ use Cake\ORM\RulesChecker;
 use Cake\ORM\Table;
 use Cake\Validation\Validator;
 
-/**
- * Loaihang Model
- *
- * @property \App\Model\Table\ChitietloaihangTable|\Cake\ORM\Association\HasMany $Chitietloaihang
- *
- * @method \App\Model\Entity\Loaihang get($primaryKey, $options = [])
- * @method \App\Model\Entity\Loaihang newEntity($data = null, array $options = [])
- * @method \App\Model\Entity\Loaihang[] newEntities(array $data, array $options = [])
- * @method \App\Model\Entity\Loaihang|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
- * @method \App\Model\Entity\Loaihang|bool saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
- * @method \App\Model\Entity\Loaihang patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
- * @method \App\Model\Entity\Loaihang[] patchEntities($entities, array $data, array $options = [])
- * @method \App\Model\Entity\Loaihang findOrCreate($search, callable $callback = null, $options = [])
- */
 class LoaihangTable extends Table
 {
 
-    /**
-     * Initialize method
-     *
-     * @param array $config The configuration for the Table.
-     * @return void
-     */
     public function initialize(array $config)
     {
         parent::initialize($config);
@@ -41,13 +21,6 @@ class LoaihangTable extends Table
             'foreignKey' => 'loaihang_id'
         ]);
     }
-
-    /**
-     * Default validation rules.
-     *
-     * @param \Cake\Validation\Validator $validator Validator instance.
-     * @return \Cake\Validation\Validator
-     */
     public function validationDefault(Validator $validator)
     {
         $validator

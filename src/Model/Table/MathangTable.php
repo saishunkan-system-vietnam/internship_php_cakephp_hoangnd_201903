3015a31b891ee191d734db51b57ffa65dbf897ce
HoangND <hoangnguyenit98@gmail.com>
2019-03-15 17:29:18 +0700

    update Model/Table/MathangTable.php
    Signed-off-by: HoangND <hoangnguyenit98@gmail.com>

--- src/Model/Table/MathangTable.php
+++ src/Model/Table/MathangTable.php
@@ -8,6 +8,10 @@ use Cake\Validation\Validator;
 
 class MathangTable extends Table
 {
-
+    public function validationDefault(Validator $validator) {
+        $validator->allowEmptyString('chitietloaihang',FALSE,'Phải chọn chi tiết loại hàng')
+                ->requirePresence('chitietloaihang','Mathang','Chi tiet mat hang can phai co');
+        return $validator;
+    }
 
 }

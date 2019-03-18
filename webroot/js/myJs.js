f73bd8e58a14b4c3e3dfc1cde472d8568ca3cf96
HoangND <hoangnguyenit98@gmail.com>
2019-03-18 17:38:42 +0700

    update myJs.js

--- webroot/js/myJs.js
+++ webroot/js/myJs.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
-     $('#hinhanhmoi').hide();
-    if ($('#loaihang').val()& $('#chitietloaihang').attr('title')==='add') {
+    $('#hinhanhmoi').hide();
+//    $.ajaxSetup({
+//        headers: {
+//            'X-CSRF-TOKEN': $('input[name="_csrfToken"]').val()
+//        }
+//    });
+    if ($('#loaihang').val() & $('#chitietloaihang').attr('title') === 'add') {
         if ($('#loaihang').val() !== 'chon') {
             loadlstchitietloaihang();
         }
@@ -9,12 +14,12 @@ $(document).ready(function () {
     $('#loaihang').change(function () {
         loadlstchitietloaihang();
     });
-    
-    $('#doianhmoi').click(function (){
+
+    $('#doianhmoi').click(function () {
         $('#doianhmoi').hide();
         $('#anh').hide();
         $('#hinhanhmoi').show();
-    })
+    });
 });
 function loadlstchitietloaihang() {
     $.ajaxSetup({
@@ -35,20 +40,20 @@ function loadlstchitietloaihang() {
 }
 function deletePhone(id) {
     var r = confirm("Xóa mặt hàng?");
-    if (r == true) {
+    if (r === true) {
         $.ajaxSetup({
-        headers: {
-            'X-CSRF-TOKEN': $('input[name="_csrfToken"]').val()
-        }
+            headers: {
+                'X-CSRF-TOKEN': $('input[name="csrfToken"]').val()
+            }
         });
         $.ajax({
-            method:'post',
-            url:"/shopdienthoai/Ajax/deletemathang",
-            data:{mathang_id:id}
-        }).done(function (rq){            
-             location.reload();
-             alert('xóa thành công');
-        }).fail(function (rq){
+            method: 'post',
+            url: "/shopdienthoai/Ajax/deletemathang",
+            data: {mathang_id: id}
+        }).done(function (rq) {
+            location.reload();
+            alert('xóa thành công');
+        }).fail(function (rq) {
             alert('xóa không thành công');
         });
     }

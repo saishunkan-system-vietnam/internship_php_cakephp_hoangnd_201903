5468b3a9c65b50768e577b1fa6e9bcc531e7acd8
HoangND <hoangnguyenit98@gmail.com>
2019-03-19 17:44:15 +0700

    add Component/validationComponen

---
+++ src/Controller/Component/validationComponent.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of getmessvalidationComponent
+ *
+ * @author HoangND
+ */
+
+namespace App\Controller\Component;
+
+use Cake\Controller\Component;
+
+class validationComponent extends Component {
+
+    public function getmessage($validation) {
+        foreach ($validation as $value) {
+            foreach ($value as $v) {
+                return $v;
+            }
+        }
+    }
+}

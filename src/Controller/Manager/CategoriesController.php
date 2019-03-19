ebb3460d96269e5c6e003e1b37439e66bbb5f1f4
hoangnd <hoangnguyenit98@gmail.com>
2019-03-19 23:19:39 +0700

    update CategoriesController

--- src/Controller/Manager/CategoriesController.php
+++ src/Controller/Manager/CategoriesController.php
@@ -6,12 +6,31 @@ use App\Controller\Manager\ManagersController;
 
 class CategoriesController extends ManagersController {
 
+    public function initialize() {
+        parent::initialize();
+        $this->loadComponent('categories');
+    }
+
     public function index() {
         
     }
 
     public function add() {
-       // echo \Cake\Routing\Router::url(null,true);die();
+        $optionSelectSubproducer= $this->categories->getSelectOption($this->categories->selectAll());
+        $this->set('option',$optionSelectSubproducer);
+        if ($this->request->isPost()) {
+            $reqCategory = $this->request->getData();
+            if (isset($reqCategory['addproducer'])) {
+                $result = $this->categories->add(['name' => $reqCategory['name'], 'parent_id' => 0]);
+                $message = ($result === true) ? 'Add successful!' : 'Add fail';
+                $this->Flash->success($message);
+            }
+            if (isset($reqCategory['addsubproducer'])) {
+                $result = $this->categories->add(['name' => $reqCategory['namesub'], 'parent_id' => $reqCategory['parent_id']]);
+                $message = ($result === true) ? 'Add successful!' : 'Add fail';
+                $this->Flash->success($message);
+            }
+        }
     }
 
 }

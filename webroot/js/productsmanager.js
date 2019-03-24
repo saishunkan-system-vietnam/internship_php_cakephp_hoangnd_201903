84d048c03dd2480937605f9b2f3cedb06af18bca
hoangnd <hoangnguyenit98@gmail.com>
2019-03-24 23:31:19 +0700

    update webroot

--- webroot/js/productsmanager.js
+++ webroot/js/productsmanager.js
@@ -5,7 +5,25 @@ $(document).ready(function () {
     $("select[name='producer']").change(function () {
         getSubproducer();
     });
-    $('#my-dialog').dialog();
+    $("input[name='addimg']").change(function () {
+        var fd = new FormData();
+        var file = $("input[name='addimg']")[0].files[0];
+        var add=($("input[name='addimg']").attr('add'))?'add':'';
+        fd.append('file', file);
+        $.ajax({
+            url: '/shopdienthoai/manager/saveimagesinram',
+            type: 'post',
+            data: fd,
+            contentType: false,
+            processData: false,
+            cache: false
+        }).done(function (rp) {
+            alert(rp);
+        });
+    });
+    $("input[name='addimg']").onclose(function (){
+        alert('dong');
+    });
 });
 function getSubproducer() {
     csrf = $('meta[name="csrfToken"]').attr('content');

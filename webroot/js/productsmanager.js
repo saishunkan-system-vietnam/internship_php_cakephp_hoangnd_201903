6e3e5437f4a750ea3d3a099c928783eed0857385
HoangND <hoangnguyenit98@gmail.com>
2019-03-25 17:41:59 +0700

    update css and js

--- webroot/js/productsmanager.js
+++ webroot/js/productsmanager.js
@@ -5,11 +5,14 @@ $(document).ready(function () {
     $("select[name='producer']").change(function () {
         getSubproducer();
     });
+    showbtn();
+
     $("input[name='addimg']").change(function () {
         var fd = new FormData();
         var file = $("input[name='addimg']")[0].files[0];
-        var add=($("input[name='addimg']").attr('add'))?'add':'';
+        var add = ($("input[name='addimg']").attr('add')) ? 'add' : '';
         fd.append('file', file);
+        fd.append('add', add);
         $.ajax({
             url: '/shopdienthoai/manager/saveimagesinram',
             type: 'post',
@@ -18,12 +21,37 @@ $(document).ready(function () {
             processData: false,
             cache: false
         }).done(function (rp) {
+            $("input[name='addimg']").attr('add', 'add');
+            $('#img').html(rp);
+            showbtn();            
+        });
+
+    });
+
+    $(document).on('click', '.remove-icon', function () {
+        var t = $(this).parent('.col-md-3');
+        t.remove();
+        $.ajax({
+            headers: {'X-CSRF-Token': $('meta[name="csrfToken"]').attr('content')},
+            url: '/shopdienthoai/manager/removeimageinram',
+            type: 'post',
+            data: {imgName: $(this).attr('imgName')}
+        }).done(function () {
+            t.remove();
+            showbtn();
+        });
+    });
+
+    $(document).on('click','button[name="save"]',function (){
+         $.ajax({
+            headers: {'X-CSRF-Token': $('meta[name="csrfToken"]').attr('content')},
+            url: '/shopdienthoai/manager/saveimages',
+            type: 'post'
+        }).done(function (rp) {
             alert(rp);
         });
     });
-    $("input[name='addimg']").onclose(function (){
-        alert('dong');
-    });
+
 });
 function getSubproducer() {
     csrf = $('meta[name="csrfToken"]').attr('content');
@@ -37,4 +65,17 @@ function getSubproducer() {
     }).fail(function () {
         alert('load fail');
     });
+}
+
+function showbtn() {
+    if ($('#img')) {
+        var count = $("#img .image").length;
+        if (count > 0) {
+            $('input[name="save"]').show();
+            $('input[name="removeall"]').show();
+        } else {
+            $('input[name="save"]').hide();
+            $('input[name="removeall"]').hide();
+        }
+    }
 }

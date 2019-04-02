151f6ac25cb34d5b12906d4ae302505f6acbf827
HoangND <hoangnguyenit98@gmail.com>
2019-04-02 08:19:47 +0700

    add model

---
+++ src/Model/Table/OrdersTable.php
@@ -0,0 +1,89 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Orders Model
+ *
+ * @property \App\Model\Table\SubaddressTable|\Cake\ORM\Association\BelongsTo $Subaddress
+ *
+ * @method \App\Model\Entity\Order get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Order newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\Order[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Order|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Order|bool saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Order patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Order[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Order findOrCreate($search, callable $callback = null, $options = [])
+ */
+class OrdersTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('orders');
+        $this->setDisplayField('id');
+        $this->setPrimaryKey('id');
+
+        $this->belongsTo('Subaddress', [
+            'foreignKey' => 'subaddress_id',
+            'joinType' => 'INNER'
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmptyString('id', 'create');
+
+        $validator
+            ->dateTime('date_time')
+            ->requirePresence('date_time', 'create')
+            ->allowEmptyDateTime('date_time', false);
+
+        $validator
+            ->integer('status')
+            ->requirePresence('status', 'create')
+            ->allowEmptyString('status', false);
+
+        $validator
+            ->scalar('note')
+            ->requirePresence('note', 'create')
+            ->allowEmptyString('note', false);
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->existsIn(['subaddress_id'], 'Subaddress'));
+
+        return $rules;
+    }
+}
